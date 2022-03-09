eddc322b3ca5ccadaa7ce8103eb64bb06a24a2bb
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-09 15:05:17 +0100

    Create Main_Frame.java

---
+++ Main_Frame.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main_Frame extends JFrame implements ActionListener, MouseListener, KeyListener{
+
+	public Main_Frame(){
+		
+	}
+
+
+}
+
+
