de85b87d497c82c77c3cd5c8dfce19148de86ba0
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-09 15:32:10 +0100

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,12 +1,20 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
-public class Frame extends JFrame implements ActionListener, MouseListener, KeyListener{
+public class Frame extends JFrame{
+
+public Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
 
 	public Frame(){
-		
+		setTitle("K-Roof");
+		setLayout(null);
+
+		setSize(screenSize.width, (int) (0.9*screenSize.height));
+		setVisible(true);
 	}
 
 
