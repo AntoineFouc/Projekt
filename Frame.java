74cd6b85db0121b216098eecf300692fbcdb3cab
Martin NIZON <nizonmartin@gmail.com>
2022-03-15 15:00:29 +0100

    fenetre principale

--- Frame.java
+++ Frame.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Image;
 import java.awt.event.*;
 
 
 public class Frame extends JFrame implements ActionListener, MouseListener, KeyListener{
 
-	public Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();	// taille de l ecran
 	final ImageIcon icon = new ImageIcon("map.png");
 	private Timer timer;
 	private int dt = 20;
@@ -17,8 +17,8 @@ public class Frame extends JFrame implements ActionListener, MouseListener, KeyL
 	public Frame(){
 		setTitle("K-Roof");
 		setLayout(null);
-		setExtendedState(this.MAXIMIZED_BOTH);
-		//setResizable(false);
+		setSize(1100,800);
+		// setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -26,7 +26,7 @@ public class Frame extends JFrame implements ActionListener, MouseListener, KeyL
 		// Panneau affichage
 		JPanel p1 = new JPanel();
 		p1.setLayout(null);
-		p1.setBounds(0, 0, 800, getSize().height);
+		p1.setBounds(0,0,800,800);
 
 		JLabel map = new JLabel(new ImageIcon("map.png"));			// image du carrefour
 		map.setBounds(0,0,800,800);
@@ -38,17 +38,14 @@ public class Frame extends JFrame implements ActionListener, MouseListener, KeyL
 		// Panneau interface
 		JPanel p2 = new JPanel();
 		p2.setLayout(null);
-		p2.setBackground(new Color(60, 60, 230));
-		p2.setBounds(800, 0, getSize().width-800, getSize().height);
+		p2.setBackground(new Color(230,230,230));
+		p2.setBounds(800,0,300,800);
 		add(p2);
 
 		start = new JButton("Commencer");
-		start.setBounds(100,50,200,50);
+		start.setBounds(50,700,200,50);
 		start.addActionListener(this);
 		p2.add(start);
-
-
-		addMouseListener(this);
 
 		timer = new Timer(dt,this);
 		time=0;
