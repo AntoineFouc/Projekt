2b5929fc88abacb3798dd9254f36589e13657c0b
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-16 14:26:44 +0100

    Update Vehicule.java

--- Vehicule.java
+++ Vehicule.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 
 public abstract class Vehicule {
-	protected int[] position = new int[2];		// x y (point en haut a gauche)
-	protected double speed;		// en pixel par ms
+	protected int[] position = new int[2];		// x y (point en haut a gauche)  A generaliser de maniere a definir cette position comme le centre du vehicule
+	protected double speed;						// en pixel par ms
 	protected int[] size = new int[2];			// taille longueur puis largeur
-	protected Road road;
+	protected Road road;						// on assigne a une route a un vehicule
 
 	
 	
@@ -13,6 +13,7 @@ public abstract class Vehicule {
 		road = r;
 	}
 
+	// methode abstraite permettant ensuite de positionner initialement le vehicule
 	public void setPosition(){}
 
 	public int[] getPosition(){
@@ -27,12 +28,15 @@ public abstract class Vehicule {
 		return road;
 	}
 
-	// pas finie
+	// Permet de changer la position du vehicule et de l'afficher au bon endroit en fonction du temps
+	// A generaliser selon les routes verticales ou horizontales
 	public void move(Graphics g, int dt){
 		position[0] = position[0] + (int) (dt*speed);
-		g.fillRect(position[0], position[1], size[0], size[1]);
+		draw(g);
 	}
 
+	// dessine le vehicule
+	// A generaliser
 	public void draw(Graphics g){
 		g.fillRect(position[0], position[1], size[0], size[1]);
 	}
