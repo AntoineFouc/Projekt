7b32f5028c4907fa60da6c8d69ba4b02762c9160
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-16 14:27:15 +0100

    Update DisplayPanel.java

--- DisplayPanel.java
+++ DisplayPanel.java
@@ -5,9 +5,9 @@ import java.awt.event.*;
 public class DisplayPanel extends JPanel implements ActionListener{
 
 	private Timer timer;
-	private int dt = 20;
-	private int time;
-	private Frame frame;
+	private int dt = 20;			// pas de tps
+	private int time;				// compteur de temps
+	private Frame frame;			// fenetre principale
 
 	public DisplayPanel(Frame f){
 		timer = new Timer(dt,this);
@@ -17,13 +17,14 @@ public class DisplayPanel extends JPanel implements ActionListener{
 
 	public void paint(Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(new ImageIcon("map.png").getImage(), 0, 0, null);
+        g2d.drawImage(new ImageIcon("map.png").getImage(), 0, 0, null);			// affichage du fond
+
         g.setColor(Color.red);
-        frame.car1.draw(g);
+        frame.car1.draw(g);										// dessine la voiture en sa position initiale
 
 
         if(time!=0){
-        	frame.car1.move(g, dt);
+        	frame.car1.move(g, dt);								// fait bouger la voiture
         }
 
 	}
@@ -32,6 +33,7 @@ public class DisplayPanel extends JPanel implements ActionListener{
 		return timer;
 	}
 
+	// repaint et met a jour le tps tous les dt
 	public void actionPerformed(ActionEvent e){
 		time += dt;
 		repaint();
