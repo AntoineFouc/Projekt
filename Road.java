eb192e38ddf54bbe37b2ae5e1cc3c50004c31ff0
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-16 14:26:28 +0100

    Update Road.java

--- Road.java
+++ Road.java
@@ -1,17 +1,16 @@
-
-
 public class Road{
 
 	private int[] startingPoint = new int[2];
 	private int[] endingPoint = new int[2];
 
-    public Road(int x1, int y1, int x2, int y2){
+    public Road(int x1, int y1, int x2, int y2){            // Point 1 de depart 2 d arrivee
     	startingPoint[0] = x1;
     	startingPoint[1] = y1;
     	endingPoint[0] = x2;
     	endingPoint[1] = y2;
     }
 
+    // getters permettant d'obtenir les points de la route
     public int getx1(){
     	return startingPoint[0];
     }
