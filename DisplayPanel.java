35d9fa0b95ec433c508a8d4b88990383da167fdd
MartinLeNizon <96054035+MartinLeNizon@users.noreply.github.com>
2022-03-16 11:23:58 +0100

    Update DisplayPanel.java

--- DisplayPanel.java
+++ DisplayPanel.java
@@ -7,17 +7,26 @@ public class DisplayPanel extends JPanel implements ActionListener{
 	private Timer timer;
 	private int dt = 20;
 	private int time;
+	private Frame frame;
 
-	public DisplayPanel(){
+	public DisplayPanel(Frame f){
 		timer = new Timer(dt,this);
 		time = 0;
+		frame = f;
 	}
 
 	public void paint(Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(new ImageIcon("map.png").getImage(), 0, 0, null);
         g.setColor(Color.red);
-        g.drawRect(355,50,20,40);
+        frame.car1.draw(g);
+
+
+        if(time!=0){
+        	// g.drawRect(0,0,500,500);
+        	frame.car1.move(g, dt);
+        }
+
 	}
 
 	public Timer getTimer(){
@@ -27,5 +36,7 @@ public class DisplayPanel extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent e){
 		time += dt;
 		repaint();
+
+
 	}
 }
