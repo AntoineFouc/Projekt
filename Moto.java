9ea353ece3eddf9b8e9bd9337a6d4eef66ef71bf
EnVoiture5imone <101265501+EnVoiture5imone@users.noreply.github.com>
2022-05-01 20:52:15 +0200

    Add files via upload

--- Moto.java
+++ Moto.java
@@ -5,8 +5,8 @@ public class Moto extends Vehicule {
 
 	public Moto(Route r, double s, double a) {
 		super(r, s, a);
-		size[0] = 43; // taille des motos : longueur
-		size[1] = 20; // largeur
+		size[0] = 32; // taille des motos : longueur
+		size[1] = 14; // largeur
 		String pictureName = "Images/moto-" + Integer.toString(Route.getOrientation()) + ".png";
 		picture = new ImageIcon(pictureName);
 		position = -size[0] / 2;
